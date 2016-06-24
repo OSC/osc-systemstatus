ccc6744b0e3b3a87ec280a986f3115d9114fc9f6
Brian McMichael <bmcmichael@osc.edu>
2016-06-24 13:48:53 -0400

    add nullclass for showqer

---
+++ app/models/showqer_not_available.rb
@@ -0,0 +1,5 @@
+class ShowqerNotAvailable < Showqer
+  def initialize
+    self.active_jobs = self.eligible_jobs = self.blocked_jobs = self.procs_used = self.procs_avail = self.nodes_used = self.nodes_avail = 0
+  end
+end
