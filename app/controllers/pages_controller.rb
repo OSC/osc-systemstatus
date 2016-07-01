a2a9c4a245b8d3488de3aa740950d964b2693703
Brian McMichael <bmcmichael@osc.edu>
2016-07-01 16:57:07 -0400

    first pass at controller modification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,41 @@
 class PagesController < ApplicationController
 
   def index
-    begin
-      @showqoakley = Showqer.new 'oakley'
-      @showqoakley.setup
-    rescue Exception => e
-      logger.error "Loading Oakley showq data failed #{e.message}"
-      logger.error e.backtrace.join("\n")
-      flash.now[:alert] = "Error loading showq data."
-      @showqoakley = ShowqerNotAvailable.new
-    end
 
-    begin
-      @showqruby = Showqer.new 'ruby'
-      @showqruby.setup
-    rescue Exception => e
-      logger.error "Loading Ruby showq data failed #{e.message}"
-      logger.error e.backtrace.join("\n")
-      flash.now[:alert] = "Error loading showq data."
-      @showqruby = ShowqerNotAvailable.new
+    @clusters = Hash.new
+
+    OodAppkit.clusters.hpc.each do |key, cluster|
+     if cluster.ganglia_server?
+       ganglia_cluster = Hash.new
+       ganglia_cluster[:name] = key
+       showqer = Showqer.new key.to_s
+       showqer.setup
+       ganglia_cluster[:showqer] = showqer
+       @clusters[key] = ganglia_cluster
+     end
     end
   end
+
+  #  begin
+  #    @showqoakley = Showqer.new 'oakley'
+  #    @showqoakley.setup
+  #  rescue Exception => e
+  #    logger.error "Loading Oakley showq data failed #{e.message}"
+  #    logger.error e.backtrace.join("\n")
+  #    flash.now[:alert] = "Error loading showq data."
+  #    @showqoakley = ShowqerNotAvailable.new
+  #  end
+  #
+  #  begin
+  #    @showqruby = Showqer.new 'ruby'
+  #    @showqruby.setup
+  #  rescue Exception => e
+  #    logger.error "Loading Ruby showq data failed #{e.message}"
+  #    logger.error e.backtrace.join("\n")
+  #    flash.now[:alert] = "Error loading showq data."
+  #    @showqruby = ShowqerNotAvailable.new
+  # end
+  #end
 
   def oakley
     @ganglia = Ganglia.new.oakley
