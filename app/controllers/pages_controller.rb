5623fc7a83a2fde05eb0eecac88c00796fe72901
Brian McMichael <bmcmichael@osc.edu>
2016-07-05 10:55:30 -0400

    remove old cluster specific controller references

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,27 +23,6 @@ class PagesController < ApplicationController
     end
   end
 
-  #  begin
-  #    @showqoakley = Showqer.new 'oakley'
-  #    @showqoakley.setup
-  #  rescue Exception => e
-  #    logger.error "Loading Oakley showq data failed #{e.message}"
-  #    logger.error e.backtrace.join("\n")
-  #    flash.now[:alert] = "Error loading showq data."
-  #    @showqoakley = ShowqerNotAvailable.new
-  #  end
-  #
-  #  begin
-  #    @showqruby = Showqer.new 'ruby'
-  #    @showqruby.setup
-  #  rescue Exception => e
-  #    logger.error "Loading Ruby showq data failed #{e.message}"
-  #    logger.error e.backtrace.join("\n")
-  #    flash.now[:alert] = "Error loading showq data."
-  #    @showqruby = ShowqerNotAvailable.new
-  # end
-  #end
-
   def oakley
     @ganglia = Ganglia.new.oakley
     render "system_status"
