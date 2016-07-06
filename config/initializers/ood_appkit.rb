4a8182a30a48f886e1f848362ffbe58c81513091
Brian McMichael <bmcmichael@osc.edu>
2016-07-06 09:35:13 -0400

    Filter list of available servers

--- config/initializers/ood_appkit.rb
+++ config/initializers/ood_appkit.rb
@@ -1,3 +1,3 @@
 # config/initializers/ood_appkit.rb
 
-OODClusters = OodAppkit.clusters.hpc
+OODClusters = OodAppkit.clusters.hpc.select { |k, c| c.ganglia_server? && c.scheduler_server? && c.scheduler_server.respond_to?(:moabhomedir) }
