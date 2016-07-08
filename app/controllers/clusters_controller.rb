0466f8537bc5662702aea9e01a70db2cdbab5103
Brian McMichael <bmcmichael@osc.edu>
2016-07-08 11:38:57 -0400

    improved 404

--- app/controllers/clusters_controller.rb
+++ app/controllers/clusters_controller.rb
@@ -39,7 +39,7 @@ class ClustersController < ApplicationController
 
   def render_404
     respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
+      format.html { render :file => "public/404", :layout => false, :status => :not_found }
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
