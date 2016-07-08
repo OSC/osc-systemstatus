e9bdc9f93e6d462d08d7ccb0f0d3fad61562964a
Brian McMichael <bmcmichael@osc.edu>
2016-07-08 09:54:13 -0400

    raise error if host invalid

--- app/controllers/clusters_controller.rb
+++ app/controllers/clusters_controller.rb
@@ -5,12 +5,15 @@ class ClustersController < ApplicationController
   end
 
   def show
-    @clusters = get_clusters
-    cluster = params[:id]
-    @ganglia = Ganglia.new(cluster.to_s)
+    begin
+      cluster = OODClusters.fetch(params[:id].to_sym)
+    rescue
+      raise "Cluster not found!" if cluster.nil?
+    end
+    @ganglia = Ganglia.new(params[:id])
     render "system_status"
   end
-  
+
   private
 
   def get_clusters
@@ -19,12 +22,11 @@ class ClustersController < ApplicationController
     OODClusters.each do |key, cluster|
       if cluster.ganglia_server?
         ganglia_cluster = Hash.new
-        ganglia_cluster[:name] = cluster.title
         begin
           showqer = Showqer.new key.to_s
           showqer.setup
         rescue Exception => e
-          logger.error "Loading Oakley showq data failed #{e.message}"
+          logger.error "Loading #{cluster.title} showq data failed #{e.message}"
           logger.error e.backtrace.join("\n")
           flash.now[:alert] = "Error loading showq data."
           showqer = ShowqerNotAvailable.new
