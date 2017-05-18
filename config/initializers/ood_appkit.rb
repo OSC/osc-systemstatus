032af6973a7671e12daa383df13161a914630956
Brian McMichael <bmcmichael@osc.edu>
2017-05-18 12:11:42 -0400

    Use OodAppkit compatible initializer

--- config/initializers/ood_appkit.rb
+++ config/initializers/ood_appkit.rb
@@ -1,8 +1,12 @@
 # config/initializers/ood_appkit.rb
 
-OODClusters = OodAppkit.clusters.select do |c|
-  c.valid? && c.hpc_cluster? &&
-    c.ganglia_server? &&
-    c.scheduler_server? &&
-    c.scheduler_server.is_a?(OodCluster::Servers::Moab)
-end.each_with_object({}) { |c, h| h[c.id] = c }
+#OODClusters = OodAppkit.clusters.select do |c|
+#  c.valid? && c.hpc_cluster? &&
+#    c.ganglia_server? &&
+#    c.scheduler_server? &&
+#    c.scheduler_server.is_a?(OodCluster::Servers::Moab)
+#end.each_with_object({}) { |c, h| h[c.id] = c }
+
+OODClusters = OodCore::Clusters.new(
+    OodAppkit.clusters.select(&:job_allow?).reject { |c| c.metadata.hidden }
+)
