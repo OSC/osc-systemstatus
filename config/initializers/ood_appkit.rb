6721e3cc9476fda503e39d4e54202ad1c677a8c6
Brian McMichael <bmcmichael@osc.edu>
2017-05-18 13:42:54 -0400

    Remove commented old initializer

--- config/initializers/ood_appkit.rb
+++ config/initializers/ood_appkit.rb
@@ -1,12 +1,5 @@
 # config/initializers/ood_appkit.rb
 
-#OODClusters = OodAppkit.clusters.select do |c|
-#  c.valid? && c.hpc_cluster? &&
-#    c.ganglia_server? &&
-#    c.scheduler_server? &&
-#    c.scheduler_server.is_a?(OodCluster::Servers::Moab)
-#end.each_with_object({}) { |c, h| h[c.id] = c }
-
 OODClusters = OodCore::Clusters.new(
     OodAppkit.clusters.select(&:job_allow?)
         .select { |c| c.custom_config[:moab] }
