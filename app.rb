22683932146af11fba766fcdb8532e82f6e4dcfb
ariel <zhu.1444@osu.edu>
2018-06-18 14:14:15 -0400

    debug

--- app.rb
+++ app.rb
@@ -113,7 +113,9 @@ get '/clusters' do
   # @ganglia = Ganglia.new(cluster)
   erb :system_status
 end
-get '/cluster' do
+get '/get' do
+  cluster = @oodclusters[:ruby] || nil
+  @ganglia = Ganglia.new(cluster)
   erb :system_status
 end
 get '/about' do
