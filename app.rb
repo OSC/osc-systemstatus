fa8cee8c49cfa695194a083ec9e1fcbd798d459c
ariel <zhu.1444@osu.edu>
2018-06-18 12:06:14 -0400

    debug

--- app.rb
+++ app.rb
@@ -106,7 +106,7 @@ end
 # end
 
 get 'clusters/:id' do
-  cluster = @oodclusters[params[:id].to_sym] || nil
+  cluster = @oodclusters[(params[:id]).to_sym] || nil
   if cluster.nil?
     File.read('404.html')
   else
