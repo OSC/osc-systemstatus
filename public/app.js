2a8281a23dbd237d55a3c8d0ae42afe5f6c2344f
ariel <zhu.1444@osu.edu>
2018-06-22 16:58:37 -0400

    move scripts back

--- public/app.js
+++
@@ -1,4 +0,0 @@
-$('[data-toggle="tooltip"]').tooltip();
-setTimeout($(".updateable:visible").each(function(){
-   return this.src = URI(this.src).setSearch("timestamp", (new Date).getTime()).toString();
-}),5000);
