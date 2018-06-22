8bd1f5ab87615e2b53ab6ac123d590b626a2b6e7
ariel <zhu.1444@osu.edu>
2018-06-22 14:18:33 -0400

    add 1.12.4 jquery and font awesome to public,  error and notfound erb page to views, clean up configuration, env files, route

--- app.rb
+++ app.rb
@@ -4,40 +4,6 @@ require 'moab'
 
 Dir[File.dirname(__FILE__) + "/lib/*.rb"].each {|file| require_relative file }
 
-configure do
-  set :environments, %w{development test production}
-  set :root, File.dirname(__FILE__)
-  set :public_folder, settings.root+"/public"
-  set :views, settings.root + "/views"
-end
-
-configure :production do
-  enable :logging
-  # set :logging, Logger::INFO
-  set :dump_errors, false
-  disable :static
-end
-
-
-# app will run dev mode by default
-configure :development do
-  enable :logging
-  set :session, false
- # set :logging, Logger::DEBUG
-  set :dump_errors, true
-end
-
-configure :test do
-  # Configure static asset server for tests with Cache-Control for performance.
-  enable :static
-  set :static_cache_control, [:public, :max_age => 3600]
-  # Raise exceptions instead of rendering exception templates.
-  # Enabled by default when environment is set to "development", disabled otherwise.
-  disable :show_exceptions
-  # Show full error reports
-  set :dump_errors, true
-end
-
 helpers do
 
   def parse_clusters
@@ -61,6 +27,14 @@ helpers do
     Rack::Utils.escape_html(text)
   end
 
+  def dashboard_title
+    ENV['OOD_DASHBOARD_TITLE'] || "Open OnDemand"
+  end
+
+  def dashboard_url
+    "/pun/sys/dashboard/"
+  end
+
 end
 
 before do
@@ -69,21 +43,17 @@ end
 
 get '/clusters/:id' do
   id=params[:id].to_sym
-  cluster = @oodclusters[id]|| nil
+  cluster = @oodclusters[id]
   if cluster.nil?
-    File.read('404.html')
+    raise Sinatra::NotFound
   else
     @ganglia = Ganglia.new(cluster)
     erb :system_status
   end
 end
 
-get '/about' do
-  erb :about
-end
-
 get '/' do
-  erb :index
+  redirect '/clusters'
 end
 
 get '/clusters' do
@@ -93,10 +63,10 @@ end
 # 404 not found
 not_found do
   status 404
-  File.read(settings.public_folder+'/404.html')
+  erb :not_found
 end
 
 # 500 internal server error
 error do
-  File.read(settings.public_folder+'/500.html')
+  erb :internal_server_error
 end
