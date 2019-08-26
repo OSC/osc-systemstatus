285112b4dbbd314a6cd398d30e55415572c6c43b
KinanAlAttar <50109313+KinanAlAttar@users.noreply.github.com>
2019-08-26 13:55:14 -0400

    Delete pbsnodes_client_not_available.rb

--- lib/pbsnodes_client_not_available.rb
+++
@@ -1,9 +0,0 @@
-class PBSNodesClientNotAvailable < PBSNodesClient
-  def initialize(id, title, message)
-    self.gpus_available = self.gpus_used = 0
-    self.cluster_id = id
-    self.cluster_title = title
-    self.error_message = message
-    self
-  end
-end
